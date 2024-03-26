931b3300ff8aa5f97c80e6688a36965799b583c2
huangqcmail@icloud.com <huangqcmail@icloud.com>
2024-03-26 16:05:10 +0800

    增加旋转动画

--- CAAnimationChainable/Classes/CAAnimation+Extension.swift
+++ CAAnimationChainable/Classes/CAAnimation+Extension.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 public extension CAAnimation {
+    
+    /// 左右摇晃
     static func swing(amplitude: CGFloat = 8, duration: CFTimeInterval = 0.1, repeatCount: Float = 2) -> CAAnimation {
         let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
         animation.values = [0, -amplitude, amplitude, 0]
         animation.duration = duration
         animation.repeatCount = repeatCount
+        return animation
+    }
+    
+    /// 旋转
+    static func rotate(angle: Float,
+                       duration: CFTimeInterval = 1,
+                       repeatCount: Float = 1,
+                       autoreverses: Bool = false,
+                       clockwise: Bool = true) -> CAAnimation {
+        let animation = CABasicAnimation(keyPath: "transform.rotation.z")
+        animation.toValue = clockwise ? angle : -angle
+        animation.duration = duration
+        animation.repeatCount = repeatCount
+        animation.autoreverses = autoreverses
         return animation
     }
 }
--- Example/CAAnimationChainable/ViewController.swift
+++ Example/CAAnimationChainable/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import CAAnimationChainable
 
 class ViewController: UIViewController {
 
+    lazy private var _view: UIView = self.initView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.initUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,5 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self._view.animaChain.add(anima: .rotate(angle: .pi * 2,
+                                                 repeatCount: .infinity)).run()
+    }
 }
 
+private extension ViewController {
+    func initUI() {
+        self.view.addSubview(self._view)
+    }
+    
+    func initView() -> UIView {
+        let view: UIView = .init(frame: .init(x: 100, y: 100, width: 50, height: 50))
+        view.backgroundColor = .red
+        return view
+    }
+}
